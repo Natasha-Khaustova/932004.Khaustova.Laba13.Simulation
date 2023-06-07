4251778996c8f6ff10b91d538f3fa2272b1a75a5
Natasha-Khaustova <45619280+Natasha-Khaustova@users.noreply.github.com>
2023-06-07 15:17:37 +0700

    KHAUSTOVA
    LAB13

---
+++ KhaustovaLab13Simulation/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace KhaustovaLab13Simulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.DollarEd = new System.Windows.Forms.NumericUpDown();
+            this.EuroEd = new System.Windows.Forms.NumericUpDown();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.DaysLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DollarEd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EuroEd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 147);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "$";
+            series1.SmartLabelStyle.CalloutLineAnchorCapStyle = System.Windows.Forms.DataVisualization.Charting.LineAnchorCapStyle.None;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "€";
+            series2.SmartLabelStyle.CalloutLineAnchorCapStyle = System.Windows.Forms.DataVisualization.Charting.LineAnchorCapStyle.None;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1056, 468);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // DollarEd
+            // 
+            this.DollarEd.DecimalPlaces = 2;
+            this.DollarEd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DollarEd.Location = new System.Drawing.Point(78, 32);
+            this.DollarEd.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.DollarEd.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.DollarEd.Name = "DollarEd";
+            this.DollarEd.Size = new System.Drawing.Size(120, 27);
+            this.DollarEd.TabIndex = 1;
+            this.DollarEd.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // EuroEd
+            // 
+            this.EuroEd.DecimalPlaces = 2;
+            this.EuroEd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EuroEd.Location = new System.Drawing.Point(78, 95);
+            this.EuroEd.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.EuroEd.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.EuroEd.Name = "EuroEd";
+            this.EuroEd.Size = new System.Drawing.Size(120, 27);
+            this.EuroEd.TabIndex = 2;
+            this.EuroEd.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartBtn.Location = new System.Drawing.Point(269, 37);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(193, 82);
+            this.StartBtn.TabIndex = 3;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "$";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "€";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(496, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Days";
+            // 
+            // DaysLbl
+            // 
+            this.DaysLbl.AutoSize = true;
+            this.DaysLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DaysLbl.Location = new System.Drawing.Point(577, 65);
+            this.DaysLbl.Name = "DaysLbl";
+            this.DaysLbl.Size = new System.Drawing.Size(23, 25);
+            this.DaysLbl.TabIndex = 7;
+            this.DaysLbl.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 627);
+            this.Controls.Add(this.DaysLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.EuroEd);
+            this.Controls.Add(this.DollarEd);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DollarEd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EuroEd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown DollarEd;
+        private System.Windows.Forms.NumericUpDown EuroEd;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label DaysLbl;
+    }
+}
+
---
+++ KhaustovaLab13Simulation/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KhaustovaLab13Simulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double mu = 0.3, sigma = 0.25;
+
+        Random rnd = new Random();
+        double FirstRV, SecondRV;
+        double omega1 = 0, omega2 = 0;
+        double X, Y;
+        const double k = 0.1;
+
+        double Euro, Dollar;
+
+        int day = 0;
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            Dollar = (double)DollarEd.Value;
+            Euro = (double)EuroEd.Value;
+
+            if (StartBtn.Text == "Start")
+            {
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                StartBtn.Text = "Stop";
+                timer1.Start();
+            }
+            else
+            {
+
+                StartBtn.Text = "Start";
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            day += 1;
+
+            Calc();
+
+            chart1.Series[0].Points.AddXY(day, Math.Round(Dollar, 2));
+            chart1.Series[1].Points.AddXY(day, Math.Round(Euro, 2));
+            DollarEd.Value = (decimal)Dollar;
+            EuroEd.Value = (decimal)Euro;
+            DaysLbl.Text = day.ToString();
+        }
+        public void Calc()
+        {
+            X = rnd.NextDouble();
+            Y = rnd.NextDouble();
+
+            FirstRV = Math.Sqrt((-2) * Math.Log(X)) * Math.Sin(2 * Math.PI * Y);
+            omega1 = (double)(Math.Sqrt(k) * FirstRV * 0.25);
+            Dollar = Dollar * Math.Exp((mu - (double)((sigma * sigma) / 2)) * k * 0.25 + (double)(sigma * omega1));
+
+            SecondRV = Math.Sqrt((-2) * Math.Log(X)) * Math.Cos(2 * Math.PI * Y);
+            omega2 = (double)(Math.Sqrt(k) * SecondRV * 0.25);
+            Euro = Euro * Math.Exp((mu - (double)((sigma * sigma) / 2)) * k * 0.25 + (double)(sigma * omega2));
+
+        }
+    }
+}
